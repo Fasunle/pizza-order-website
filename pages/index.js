9893a5ddddedb1ace96e8d614b25cee46edde5db
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-01-09 14:41:10 +0100

    changed icon

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Food Order App in Uyo</title>
         <meta name="description" content="the best pizza in Nigeria" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/img/logo.png" />
       </Head>
 
       <main className={styles.main}>
@@ -22,10 +22,19 @@ export default function Home() {
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer">
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by Kehinde Fasunle
+          <svg viewBox="0 0 48 48" className={styles.logo}>
+            <title>Nigeria</title>
+            <g>
+              <rect x="16" y="6" fill="#E6E6E6" width="16" height="36"></rect>{" "}
+              <path
+                fill="#078754"
+                d="M48,40c0,1.105-0.895,2-2,2H32V6h14c1.105,0,2,0.895,2,2V40z"></path>
+              <path
+                fill="#078754"
+                d="M16,42H2c-1.105,0-2-0.895-2-2V8c0-1.105,0.895-2,2-2h14V42z"></path>
+            </g>
+          </svg>
         </a>
       </footer>
     </div>

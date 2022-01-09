de88f511a2152a88fa04b4b6ce9c1b28e8a5f4e1
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-01-09 15:09:37 +0100

    Add layout to the ui

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const styles = {
+  footer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
+
+const Footer = () => {
+  return <footer style={styles.footer}>Powered by Kehinde Fasunle</footer>;
+};
+
+export default Footer;

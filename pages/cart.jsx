27102d9e9849bb93e415c9e1f5b447a808eb72b3
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-02-03 12:12:15 +0100

    Refactor Cart into #Table component

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import CartTable from "../components/CartTable";
 import styles from "../styles/Cart.module.css";
 import { cartTotalPrice, getDiscount } from "../utils";
 
@@ -31,47 +32,7 @@ const cart = [
 const Cart = () => (
   <div className={styles.container}>
     <div className={styles.left}>
-      <table className={styles.table}>
-        <thead>
-          <th>Product</th>
-          <th>Name</th>
-          <th>Extra</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Total</th>
-        </thead>
-        <tbody>
-          {cart.map((cartItem, id) => (
-            <tr key={id}>
-              <td>
-                <div className={styles.imgContainer}>
-                  <Image
-                    src="/img/pizza.png"
-                    alt="product image"
-                    layout="fill"
-                    objectFit="cover"
-                  />
-                </div>
-              </td>
-              <td>
-                <span className={styles.name}>{cartItem.name}</span>
-              </td>
-              <td>
-                <span className={styles.extra}>{cartItem.extra}</span>
-              </td>
-              <td>
-                <span className={styles.price}>{cartItem.price}</span>
-              </td>
-              <td>
-                <span className={styles.quantity}>{cartItem.quantity}</span>
-              </td>
-              <td>
-                <span className={styles.total}>{cartItem.total}</span>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <CartTable cart={cart} />
     </div>
     <div className={styles.right}>
       <h2 className={styles.cartTitle}>Cart Total</h2>
